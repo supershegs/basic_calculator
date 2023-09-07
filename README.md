96b37f79e229674b1358074b71da6aa29b64393a
supershegs <segsamos@gmail.com>
2023-09-07 14:01:32 +0100

    Create a minimal Flask web application that functions as a basic calculator

---
+++ README.md
@@ -0,0 +1,15 @@
+Assignment: Basic Flask Calculator 
+Create a minimal Flask web application that functions as a basic calculator. Instructions: 
+1. Setup: 
+- Install Flask if it's not already installed on your system. 
+- Create a new directory for your project and set up a virtual environment. 
+2. Create the Flask Application: 
+- Build a Flask web application that has one route ("/") to serve a simple HTML form. - The form should include two input fields for numbers, a dropdown menu for operations (addition, subtraction, multiplication, or division), and a "Calculate" button. 
+3. Form Handling: 
+- Implement form handling for the calculation form. 
+- When the user submits the form, extract the user's input (two numbers and the selected operation). 
+- Calculate the result based on the selected operation. 
+4. Result Display: 
+- Display the calculation result on the same page ("/") beneath the form, or you can create separate route ("/result") to display the result. 
+5. Styling (Optional): 
+- Add minimal CSS styling to make the form and result visually appealing.
---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+
+# app = Flask(__name__)
+
+app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
+
+@app.route("/", methods=['GET', 'POST'])
+def basic_calc():
+    num_1,num_2,operations,result,err= None,None,None,None,None
+    if request.method == 'POST':
+        num_1 = request.form['num_1'] # num_1 = float(request.form.get('num_1'))
+        num_2 = request.form['num_2']
+        operation = request.form['operations'] 
+        if not num_1.isdigit() or not num_2.isdigit() :
+            err = 'The entered value is not digit/number. \n Both inputs must be numeric!'
+        
+        else:
+            num_1 = float(num_1)
+            num_2 = float(num_2)
+            if operation == '+':
+                result = num_1 + num_2
+            elif operation == '-':
+                result = num_1 - num_2
+            elif operation == '*':
+                result = num_1 * num_2
+            elif operation == '/':
+                if num_2 == 0.0:
+                    error_msg  = 'Division by zero is not allowed.'                    
+                    return render_template('index.html', error_msg= error_msg)
+                else:
+                    result = num_1 / num_2
+                result = num_1 / num_2
+            
+            else:
+                 err = 'Please select a valid operation sign (+, -, *, /)'
+    # display_result = '{} {} {} = {}' .format(num_1, operations ,num_2,result)
+    display_result = f'{num_1} {operation} {num_2} = {result}' if result is not None else ''
+    error_msg = err
+    return render_template('index.html', result = result,display_result= display_result, error_msg = error_msg)
+ 
+if __name__=='__main__':
+    app.run(debug = True)
